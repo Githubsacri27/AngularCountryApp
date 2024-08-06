b172d6e9c69569aa15d83ab35b2901ae22cca91a
Rubén <rsacri27@hotmail.com>
2024-08-06 10:29:31 +0200

    create loading-spinner c

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of, map } from 'rxjs';
+import { catchError, Observable, of, map, delay } from 'rxjs';
 
 import { Country } from '../interfaces/country';
 
@@ -15,6 +15,7 @@ export class CountriesService {
     return this.http.get<Country[]>(url)
     .pipe(
       catchError( () => of([]) ),
+      delay(2000)
     );
     
 
